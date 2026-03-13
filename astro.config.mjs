55c78318b4fe5bc76550deec758986b1bc120a7c
Matt Wood <designmonkeyboy@gmail.com>
2026-03-13 08:41:54 -0600

    Add preact component, add dark/light mode toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mathew-wood.netlify.app/"
+  site: "https://mathew-wood.netlify.app/",
+  integrations: [preact()]
 });
-
